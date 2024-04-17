14de1a28b0c055d4813a38c606280cd4bfa7c8b6
hmzi67 <hamzawaheed057@gmail.com>
2024-04-18 02:54:46 +0500

    Hamza: adding password input field and icons

--- app/src/main/java/io/github/uxlabspk/neuroscape/views/LoginScreen.kt
+++ app/src/main/java/io/github/uxlabspk/neuroscape/views/LoginScreen.kt
@@ -2,37 +2,27 @@ package io.github.uxlabspk.neuroscape.views
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.Email
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import io.github.uxlabspk.neuroscape.views.components.TopBar
+import io.github.uxlabspk.neuroscape.views.components.UserInput
 
 
 @Composable
 fun LoginScreen() {
     Column {
         TopBar(text = "", modifier = Modifier)
-        Column(horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center) {
-            TextField(
-                value = "",
-                onValueChange = {},
-                colors = TextFieldDefaults.colors(
-                    focusedIndicatorColor = Color.Transparent,
-
-                ),
-                placeholder = {
-                    Text(text = "sdfsdf")
-                },
-                shape = RoundedCornerShape(8.dp)
-
-            )
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            UserInput(text = "Username", inputIcon = Icons.Default.Email)
+            UserInput(text = "Username", inputIcon = Icons.Default.AccountBox)
         }
     }
 }
--- app/src/main/java/io/github/uxlabspk/neuroscape/views/components/UserInput.kt
+++ app/src/main/java/io/github/uxlabspk/neuroscape/views/components/UserInput.kt
@@ -1,6 +1,7 @@
 package io.github.uxlabspk.neuroscape.views.components
 
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Email
 import androidx.compose.material3.Text
@@ -10,17 +11,27 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import io.github.uxlabspk.neuroscape.R
 
 
 @Composable
-fun TextFieldFun(
-    text: String
+fun UserInput(
+    text: String,
+    inputIcon: ImageVector
 
 ) {
     var textState by remember {
@@ -33,7 +44,7 @@ fun TextFieldFun(
         modifier = Modifier,
         label = { Text(text) },
         leadingIcon = {
-            Icon(imageVector = Icons.Default.Email, contentDescription = "Email Icon")
+            Icon(imageVector = inputIcon, contentDescription = "Email Icon")
         },
         colors = TextFieldDefaults.colors(
             unfocusedIndicatorColor = Color.Transparent,
@@ -48,13 +59,71 @@ fun TextFieldFun(
         ),
         shape = RoundedCornerShape(25.dp),
         singleLine = true,
+
     )
 }
 
+@Composable
+fun PasswordInput() {
+    var password by rememberSaveable {
+        mutableStateOf("")
+    }
+    var passwordVisibility by remember {
+        mutableStateOf(false)
+    }
+    var icon = if(passwordVisibility)
+        painterResource(id = R.drawable.ic_visible)
+    else
+        painterResource(id = R.drawable.ic_invisible)
+    TextField(
+        value = password,
+        onValueChange = {
+            password = it
+        },
+        label = { Text(text = "Password")},
+        leadingIcon = {
+            Icon(painter = painterResource(id = R.drawable.ic_password), contentDescription = "Password Icon")
+        },
+        trailingIcon = {
+            IconButton(onClick = {
+                passwordVisibility = !passwordVisibility
+            }) {
+                Icon(
+                    painter = icon,
+                    contentDescription = "visibility icon"
+                )
+            }
+        },
+        colors = TextFieldDefaults.colors(
+            unfocusedIndicatorColor = Color.Transparent,
+            unfocusedLabelColor = Color.DarkGray,
+            focusedLabelColor = Color.DarkGray,
+            focusedLeadingIconColor = Color.Black,
+            focusedTextColor = Color.Black,
+            unfocusedTextColor = Color.DarkGray,
+            focusedIndicatorColor = Color.Transparent,
+            focusedContainerColor = Color.LightGray,
+            cursorColor = Color.Black
+        ),
+        shape = RoundedCornerShape(25.dp),
+        singleLine = true,
+        keyboardOptions = KeyboardOptions(
+            keyboardType = KeyboardType.Password
+        ),
+        visualTransformation = if(passwordVisibility) VisualTransformation.None
+        else PasswordVisualTransformation()
+    )
+}
+
+@Preview
+@Composable
+fun PasswordInputPreview() {
+    PasswordInput()
+}
 
 @Preview
 @Composable
 fun PreviewTextField () {
-    TextFieldFun(text = "Username")
+    UserInput(text = "Username", inputIcon = Icons.Default.Email)
 
 }
