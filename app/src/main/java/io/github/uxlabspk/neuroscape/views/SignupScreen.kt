240af60f3fde50bd0725f8c2a459654556b2b0cd
hmzi67 <hamzawaheed057@gmail.com>
2024-04-19 20:44:09 +0500

    Hamza: Signup page

---
+++ app/src/main/java/io/github/uxlabspk/neuroscape/views/SignupScreen.kt
@@ -0,0 +1,20 @@
+package io.github.uxlabspk.neuroscape.views
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun SignupScreen() {
+    Column() {
+        Image(painter = , contentDescription = null)
+    }
+}
+
+
+@Preview
+@Composable
+fun LoginScreenPreview() {
+    SignupScreen()
+}
