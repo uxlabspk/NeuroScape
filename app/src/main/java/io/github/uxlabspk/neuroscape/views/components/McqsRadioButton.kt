523803aad8daaaf169d576e1f9fab9c0d73fb4bf
Uxlabspk <muhammadnaveedcis@gmail.com>
2024-04-29 15:55:37 +0500

    Naveed : Updating the Questions Screen.

--- app/src/main/java/io/github/uxlabspk/neuroscape/views/components/McqsRadioButton.kt
+++ app/src/main/java/io/github/uxlabspk/neuroscape/views/components/McqsRadioButton.kt
@@ -99,7 +99,7 @@ fun CustomRadioGroup(
 @Preview
 @Composable
 fun McqsPreview() {
-    McqsRadioButton(option1 = "Hamza", option2 = "Naveed", option3 = "CEO", option4 = "CTO", option5 = "sdf")
+    McqsRadioButton(option1 = "Hamza Waheed", option2 = "Naveed", option3 = "CEO", option4 = "CTO", option5 = "sdf")
 
 }
 
