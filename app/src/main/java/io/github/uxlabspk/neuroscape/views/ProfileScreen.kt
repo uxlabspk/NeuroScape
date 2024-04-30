d11429bb82cbe9cf42f065f04e5447fa8df17289
Uxlabspk <muhammadnaveedcis@gmail.com>
2024-04-30 16:44:15 +0500

    Naveed : Updating Profile Screen

--- app/src/main/java/io/github/uxlabspk/neuroscape/views/ProfileScreen.kt
+++ app/src/main/java/io/github/uxlabspk/neuroscape/views/ProfileScreen.kt
@@ -1,5 +1,6 @@
 package io.github.uxlabspk.neuroscape.views
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -14,10 +15,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import io.github.uxlabspk.neuroscape.views.components.AltButton
+import io.github.uxlabspk.neuroscape.views.components.PrimaryButton
+import io.github.uxlabspk.neuroscape.views.components.SecondaryButton
 import io.github.uxlabspk.neuroscape.views.components.TopBar
 import io.github.uxlabspk.neuroscape.views.components.UserInput
 
@@ -29,10 +33,13 @@ fun ProfileScreen(
     Column(
         modifier = Modifier
             .fillMaxSize()
+            .background(Color.White)
     ) {
-        TopBar(text = "Profile", modifier = Modifier, {})
+        TopBar(text = "Profile", modifier = Modifier.height(56.dp), {})
         Column (
-            modifier = Modifier.fillMaxWidth().padding(horizontal = 12.dp, vertical = 20.dp),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 20.dp, vertical = 20.dp),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Icon(
@@ -43,13 +50,16 @@ fun ProfileScreen(
                     .padding(top = 20.dp),
                 contentDescription = "Profile Icon")
             Text(text = userName, fontSize = 26.sp)
-            UserInput(text = userEmail, inputIcon = Icons.Default.Email,modifier = Modifier.padding(top = 60.dp), isEnabled = false)
-            AltButton(text = "Logout", modifier = Modifier.fillMaxWidth().padding(top = 10.dp), {})
+            Text(text = userEmail, fontSize = 16.sp)
+//            UserInput(text = userEmail, inputIcon = Icons.Default.Email,modifier = Modifier.padding(top = 60.dp), isEnabled = false)
+            //AltButton(text = "Logout", modifier = Modifier.fillMaxWidth().padding(vertical = 20.dp), {})
+            SecondaryButton("View Reports", modifier = Modifier.fillMaxWidth().padding(top = 50.dp), {})
+            SecondaryButton("Logout", modifier = Modifier.fillMaxWidth(), {})
         }
     }
 }
 
-@Preview(showBackground = true, widthDp = 330, heightDp = 880)
+@Preview(showBackground = true, widthDp = 430, heightDp = 880)
 @Composable
 fun PreviewProfile(){
     ProfileScreen(userName = "Hamza Waheed", userEmail = "Hamzawaheed057@gmail.com")
