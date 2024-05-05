51c53c0585ab75c8359794c55476dbd332ab02f0
Uxlabspk <muhammadnaveedcis@gmail.com>
2024-05-05 14:38:13 +0500

    Naveed : Adding NavHost Support

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,4 +66,8 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    implementation("androidx.navigation:navigation-compose:2.4.0-alpha05")
+
+
 }
--- app/src/main/java/io/github/uxlabspk/neuroscape/MainActivity.kt
+++ app/src/main/java/io/github/uxlabspk/neuroscape/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
 import io.github.uxlabspk.neuroscape.ui.theme.NeuroScapeTheme
 import io.github.uxlabspk.neuroscape.views.QuestionsScreen
 
@@ -22,7 +23,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    QuestionsScreen()
+
                 }
             }
         }
