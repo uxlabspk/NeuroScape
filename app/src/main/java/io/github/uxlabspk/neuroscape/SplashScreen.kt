2e4d8ed3ace43b66b830f344cda4ab03e61bf254
hmzi67 <hamzawaheed057@gmail.com>
2024-05-10 19:28:40 +0500

    Hamza: Firebase offline persistence enabled

--- app/src/main/java/io/github/uxlabspk/neuroscape/SplashScreen.kt
+++ app/src/main/java/io/github/uxlabspk/neuroscape/SplashScreen.kt
@@ -6,12 +6,15 @@ import android.os.Handler
 import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.FirebaseDatabase
 
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_splash_screen)
+
+        FirebaseDatabase.getInstance().setPersistenceEnabled(true)
 
         Handler(Looper.getMainLooper()).postDelayed({
             val intent = Intent(this, MainActivity::class.java)
