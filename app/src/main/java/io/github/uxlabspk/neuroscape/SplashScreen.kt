a965fd20c538bbd4e397d60af274e2e157e69222
hmzi67 <hamzawaheed057@gmail.com>
2024-05-10 19:17:08 +0500

    Hamza: Splash Screen Added

---
+++ app/src/main/java/io/github/uxlabspk/neuroscape/SplashScreen.kt
@@ -0,0 +1,22 @@
+package io.github.uxlabspk.neuroscape
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 1000)
+    }
+}
